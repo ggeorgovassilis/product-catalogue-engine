a930d35fd6de2ce9ebd6fcaa894b676b85737563
g.georgovassilis@gmail.com <g.georgovassilis@gmail.com@7dfbd3b7-070e-d422-0b9d-1f61dcddcaa3>
2011-09-14 12:52:08 +0000

    Moved PI/web to private repositories

---
+++ product-catalogue-engine/test/org/pce/database/AllTests.java
@@ -0,0 +1,11 @@
+package org.pce.database;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ ProductDatabaseTest.class, TestSupport.class })
+public class AllTests {
+
+}
